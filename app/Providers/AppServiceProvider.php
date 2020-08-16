b99f34edddef4b887c06910cf3de6efc40ab6cef
Mohamed Nagy <5763766-Mohamed-Nagy@users.noreply.gitlab.com>
2020-08-16 14:17:51 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Setting ;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
