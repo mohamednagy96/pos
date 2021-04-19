662024a38169dac49d4d484c2bbe3677877cd892
Mohamed-Nagy <mohamednagy9660@gmail.com>
2021-04-19 19:55:13 +0200

    login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Models\Setting ;
+use Exception;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,20 +26,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         // $settings=Setting::pluck('value','key')->toArray();
-           $settings = Setting::all('key', 'value')
-           ->keyBy('key')
-           ->transform(function ($setting) {
-               return $setting->value;
-           })->toArray();
-        // dd($settings);
-
-           config([
-               'settings' => $settings
-               ]);
-            //    dd(config('settings'));
-
-       config(['app.name' => config('settings.app_name')]);
-
+        try{
+            $settings = Setting::all('key', 'value')
+            ->keyBy('key')
+            ->transform(function ($setting) {
+                return $setting->value;
+            })->toArray();
+         // dd($settings);
+ 
+            config([
+                'settings' => $settings
+                ]);
+             //    dd(config('settings'));
+ 
+        config(['app.name' => config('settings.app_name')]);
+ 
+        }catch(Exception $e){}
+         
     }
 }
